e001ba270551dcf29c3dffeab0c1b99bb30b112a
sna-binar <snarizky@binar.co.id>
2025-08-04 23:37:44 +0700

    add checkout flow healenium case

--- src/test/java/SaucedemoCheckoutHealenium.java
+++ src/test/java/SaucedemoCheckoutHealenium.java
@@ -1,4 +1,5 @@
 import com.epam.healenium.SelfHealingDriver;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -33,6 +34,17 @@ public class SaucedemoCheckoutHealenium {
         // Tanpa auto-healing, ini akan gagal karena ID telah diubah
         driver.findElement(By.id("checkout")).click();
         System.out.println("Checkout berhasil meskipun ID diubah.");
+
+        // Input checkout info
+        driver.findElement(By.id("first-name")).sendKeys("QA");
+        driver.findElement(By.id("last-name")).sendKeys("Engineer");
+        driver.findElement(By.id("postal-code")).sendKeys("12345");
+        driver.findElement(By.id("continue")).click();
+
+        driver.findElement(By.id("finish")).click();
+        String thankYouText = driver.findElement(By.cssSelector("h2.complete-header")).getText();
+        Assert.assertEquals("Thank you for your order!", thankYouText);
+        driver.findElement(By.id("back-to-products")).isDisplayed();
 
         driver.quit();
     }
